08a393aac09f463104c2caa0310b813ba03634a3
snowy-dong <99186652@qq.com>
2018-03-28 17:31:12 +0800

    add

--- backend/routes/permiss.js
+++ backend/routes/permiss.js
@@ -1,23 +1,58 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../public/javascripts/mysql.js');  
+
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log(1111)
   console.log(req.body)
-  var sql = `insert into permiss values(0,"`+ req.body.name +`" ,"`+req.body.code+`");`;  
-  db.query(sql, function(err, results, fields){  
-    if (err) throw err;
-    // console.log('The solution is: ', results[0].solution);
-    res.send(results);
-  });  
+  count();
+  function count(){
+    let sqlonlycount = `select count(1) from  permiss where permiss_code = "`+ req.body.name +`" or permiss_name = "`+req.body.code+`";`;
+    db.query(sqlonlycount, function(err, results, fields){  
+      if (err) throw err;
+      console.log(results[0]['count(1)'])
+      if(results[0]['count(1)'] > 0){
+         res.send({
+           code: 'P001',
+           msg:"permiss_code or permiss_name  已存在"
+         });
+      }else{
+        return insert();
+      }
+    });  
+  }
+  function insert (){
+  let sql = `insert into permiss values(0,"`+ req.body.name +`" ,"`+req.body.code+`");`;  
+    db.query(sql, function(err, results, fields){  
+      if (err) throw err;
+      res.send({
+        code: 'S200',
+        msg:""
+      });
+    });
+  }
 });
 router.get('/list', function(req, res, next) {
-  var sql = `select * from permiss;`;  
-  db.query(sql, function(err, results, fields){  
-    if (err) throw err;
-    // console.log('The solution is: ', results[0].solution);
-    res.send(results);
-  });  
+  var sql = `select *  from permiss limit `+(req.query.pageNo-1)*req.query.pageSize +`,`+req.query.pageSize+`;`;  
+  var sqlCount = `select count(1)  from permiss;`;  
+  let data ={
+    count:0,
+    list:[]
+  }
+  count();
+  function count(){
+    db.query(sqlCount, function(err, results, fields){  
+      if (err) throw err;
+      data.count = results[0]['count(1)']
+      page();
+    });
+  }
+  function page(){
+    db.query(sql, function(err, results, fields){  
+      if (err) throw err;
+      data.list = results
+      res.send(data);
+    });
+  }
 });
 module.exports = router;
