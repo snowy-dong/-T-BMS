c2fd5d27ed10416b49c5ed58fcb7a7846d057150
snowy-dong <99186652@qq.com>
2018-04-03 22:32:02 +0800

    update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,46 +1,63 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../public/javascripts/mysql.js');  
-var rd = require('../public/javascripts/redis.js');  
+var db = require('../public/javascripts/mysql.js');
+var redisClient = require('../public/javascripts/redis.js').redisClient;
+var jwt = require('jsonwebtoken');
+var secret = require('../public/javascripts/secret');
+var tokenManager = require('../public/javascripts/token_manager');
+
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  console.log(req.body)
+router.post('/', function (req, res, next) {
   get();
-  function get(){
-    rd.client.hget("hash key",req.body.name,function(redisRes, reply){
+
+  function get() {
+    redisClient.get(req.body.name, function (redisRes, reply) {
       console.log(reply)
-      if(reply){
-        res.send({code:'200',msg:reply});
-      }else{
+      if (reply) {
+        res.send({
+          code: '200',
+          msg: 'reply' + reply
+        });
+      } else {
         count()
       }
-     });
+    });
   }
- function set(code){
-  rd.client.hset("hash key",req.body.name,code,function(redisRes, reply){
-    console.log(reply)
-    if(reply){
-      res.send({code:'200',msg:code});
-    }
-   });
- }
- function count(){
-  let sqlonlycount = `select count(1) from  user where user_name = "`+ req.body.name +`" and password = "`+req.body.password+`";`;
-  db.query(sqlonlycount, function(err, results, fields){  
-    if (err) throw err;
-    console.log(results[0]['count(1)'])
-    if(results[0]['count(1)'] > 0){
-      return set("1234567890")
-       res.send({
-         code: 'S200',
-         msg:"登录成功"
-       });
-    }else{
-      res.send({code:'S401',msg:'账号或密码错误！'});
-    }
-  });  
-}
+
+  function set(code) {
+    redisClient.set(req.body.name, code, function (redisRes, reply) {
+      if (reply) {
+        redisClient.expire(req.body.name, 30);
+        res.send({
+          code: '200',
+          msg: code
+        });
+      }
+    });
+  }
+
+  function count() {
+    let sqlonlycount = `select count(1) from  user where user_name = "` + req.body.name + `" and password = "` + req.body.password + `";`;
+    db.query(sqlonlycount, function (err, results, fields) {
+      if (err) throw err;
+      console.log('fields')
+      console.log(results[0])
+      if (results[0]['count(1)'] > 0) {
+        var token = jwt.sign({
+          name: req.body.name
+        }, secret.secretToken, {
+          expiresIn: tokenManager.TOKEN_EXPIRATION
+        });
+        return set(token)
+      } else {
+        res.send({
+          code: 'S401',
+          msg: '账号或密码错误！'
+        });
+      }
+    });
+  }
 });
 
 module.exports = router;
