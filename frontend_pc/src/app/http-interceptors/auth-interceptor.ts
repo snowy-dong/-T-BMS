a78fdd06917ea93fc5f1066cd1e96908b4e6e77d
snowy-dong <99186652@qq.com>
2018-04-11 14:58:21 +0800

    更新

--- frontend_pc/src/app/http-interceptors/auth-interceptor.ts
+++ frontend_pc/src/app/http-interceptors/auth-interceptor.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpEvent,HttpInterceptor,HttpHandler,HttpRequest,HttpResponse} from "@angular/common/http";
 import { Observable} from "rxjs/Observable";
 import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
-import { catchError } from 'rxjs/operators';
-import { mergeMap } from 'rxjs/operators';
+import { catchError, mergeMap } from 'rxjs/operators';
 import { AuthService } from '../auth.service';
 import {Router} from "@angular/router"
 @Injectable()
