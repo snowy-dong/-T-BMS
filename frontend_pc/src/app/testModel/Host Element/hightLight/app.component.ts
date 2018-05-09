9c0fd37c289b9d6056eb0c8d2d721109328fd8f6
snowy-dong <99186652@qq.com>
2018-05-09 14:13:30 +0800

    remove file

--- frontend_pc/src/app/testModel/Host Element/hightLight/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component} from '@angular/core';
-
-@Component({
-  selector: 'exe-app',
-  template: `
-    <h4 exeHighlight>点击该区域，元素会被高亮。点击其它区域，元素会取消高亮</h4>
-  `
-})
-export class AppComponent {}
