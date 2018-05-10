2c75f905e738404c982d738cd733d18b486edb9b
snowy-dong <99186652@qq.com>
2018-05-10 14:57:40 +0800

    更新文件上传

--- frontend_pc/src/app/active/active.module.ts
+++ frontend_pc/src/app/active/active.module.ts
@@ -1,10 +1,19 @@
 import { NgModule }           from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { ActiveComponent }     from './active.component';
 import { ActiveService }       from './active.service';
 import { ActiveRoutingModule } from './active.routing.module';
+// 文件上传
+import { NgUploaderModule } from 'ngx-uploader';
 
 @NgModule({
-  imports:      [ ActiveRoutingModule ],
+  imports:      [
+    ActiveRoutingModule,
+    FormsModule,
+    CommonModule,
+    NgUploaderModule
+  ],
   declarations: [ ActiveComponent ],
   providers:    [ ActiveService ]
 })
